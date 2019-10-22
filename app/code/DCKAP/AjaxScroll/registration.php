4baaac5a7a8f81cfb4281c21c789b6d82cbf8d49
amitvaghelaa <amit@rocktechnolabs.com>
2019-10-22 12:53:35 +0530

    chnge in app

--- app/code/DCKAP/AjaxScroll/registration.php
+++ app/code/DCKAP/AjaxScroll/registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
 \Magento\Framework\Component\ComponentRegistrar::MODULE,
-'DCKAP_AjaxScroll',
+'DCKAP_AjaxScroll',     
 __DIR__
 );
